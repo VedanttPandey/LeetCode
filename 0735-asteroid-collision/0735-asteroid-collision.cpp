98e7503478d6367e1bb2f9fd8c72dbd09401a3ba
Vedant Pandey <pandeyvedant97@gmail.com>
2025-05-16 10:32:15 +0530

    Time: 0 ms (100%), Space: 22.2 MB (19.04%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& a) {
+        stack<int> st;
+        st.push(a[0]);
+        for(int i=1;i<a.size();++i){
+            int curr=a[i];
+            int destroyed=false;
+            int ok=1;
+            while (!st.empty() && st.top() > 0 && curr < 0){//will collide
+            int top=st.top();
+            if(abs(top)>abs(curr)){//top will will win and curr will become top
+                // curr=top;
+                destroyed=1;
+                // st.pop(); 
+                break;
+            }
+            else if(abs(top)<abs(curr)){//curr will win and will pop the stack if not empty else will push curr in the stack
+                st.pop();
+                continue;
+            }
+            else{//pop the stack 
+                st.pop();
+                destroyed=1;
+                break;
+            }
+            }
+            if(!destroyed)st.push(curr);
+        }
+         vector<int> ans(st.size());
+        for (int i = st.size() - 1; i >= 0; --i) {
+            ans[i] = st.top();
+            st.pop();
+        }
+        return ans;
+    }
+};
