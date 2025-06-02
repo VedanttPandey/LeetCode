0c17ef0e9447ecad5b6d8d5eb23c03892700da97
Vedant Pandey <pandeyvedant97@gmail.com>
2025-06-02 10:43:37 +0530

    Time: 429 ms (5.06%), Space: 22.7 MB (70.62%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        int n = ratings.size();
-        vector<int> candies(n, 1);
-
-        for(int i = 1; i < n; ++i) {
-            if(ratings[i] > ratings[i - 1]) {
-                candies[i] = candies[i - 1] + 1;
+        vector<int> candies(ratings.size(),1);
+        // fill(candies.begin(),candies.end(),1);
+        for(auto &i:candies)cout<<i<<" ";
+        for(int i=1;i<ratings.size();++i){
+            if(ratings[i]>ratings[i-1]){
+                if(candies[i]==candies[i-1])candies[i]+=1;
+                else if(candies[i]<candies[i-1])candies[i]=candies[i-1]+1;
             }
+            // else if(ratings[i]==ratings[i-1] && candies[i]<candies[i-1])candies[i]=candies[i-1];
         }
-
-        for(int i = n - 2; i >= 0; --i) {
-            if(ratings[i] > ratings[i + 1]) {
-                candies[i] = max(candies[i], candies[i + 1] + 1);
+        for(int i=ratings.size()-2;i>=0;--i){
+            if(ratings[i]>ratings[i+1]){
+                if(candies[i]==candies[i+1])candies[i]+=1;
+                else if(candies[i]<candies[i+1])candies[i]=candies[i+1]+1;
             }
+            // else if(ratings[i]==ratings[i+1] && candies[i]<candies[i+1])candies[i]=candies[i+1];
         }
-
-        return accumulate(candies.begin(), candies.end(), 0);
+        for(auto &i:candies)cout<<i<<" ";
+        return accumulate(candies.begin(),candies.end(),0ll);
     }
 };
