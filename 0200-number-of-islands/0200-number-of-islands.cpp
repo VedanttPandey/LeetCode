7a2f49630eb31bdcdd26431cb0c50a53036845ce
Vedant Pandey <pandeyvedant97@gmail.com>
2025-07-14 19:05:51 +0530

    Time: 21 ms (91.69%), Space: 16.2 MB (89.77%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
 vector<int> row={-1,1,0,0};
-vector<int> col={0,0,-1,1};
-queue<pair<int,int>> q;
+vector<int> col={0,0,1,-1};
 
-
+void dfs(vector<vector<char>>& a,int i,int j,int n,int m){
+    if(i<0 || i>=n || j<0 || j>=m || a[i][j]=='0'){
+        return;
+    }
+    a[i][j]='0';
+    dfs(a,i+1,j,n,m);
+    dfs(a,i,j+1,n,m);
+    dfs(a,i-1,j,n,m);
+    dfs(a,i,j-1,n,m);
+}
     int numIslands(vector<vector<char>>& grid) {
-        int n=grid.size(),m=grid[0].size();
-        int ans=0;
-
+        int n=grid.size(),m=grid[0].size(),ans=0;
         for(int i=0;i<n;++i){
             for(int j=0;j<m;++j){
                 if(grid[i][j]=='1'){
                     ans++;
-                    q.push({i,j});
-                    grid[i][j]='0';
-                    while(!q.empty()){
-                        auto [l,r]=q.front();
-                        q.pop();
-                        for(int roww=0;roww<row.size();++roww){
-                                int nr=l+row[roww],nc=r+col[roww];
-                                if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]=='1'){
-                                    q.push({nr,nc});
-                                    grid[nr][nc]='0';
-                                }
-                        }
-                    }
-            }
+                    dfs(grid,i,j,n,m);
+                }
             }
         }
         return ans;
